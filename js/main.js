2572e050486e9622fd539312294fd655cf937eca
JuraevStudio <blogitcodex@gmail.com>
2025-08-26 04:05:38 +0500

    edit page

--- js/main.js
+++ js/main.js
@@ -1,36 +1,16 @@
-// Lenis
-const lenis = new Lenis({
-    wrapper: document.querySelector('.scroll-container'),
-    lerp: 0.1,
-    duration: 1.2,
-    smoothTouch: true
-});
-lenis.on('scroll', e => {
-    console.log(e);
-});
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
-// Animate on Scroll
-const animatedElements = document.querySelectorAll('.animate-on-scroll');
-const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.1
-};
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('is-visible');
-            observer.unobserve(entry.target);
-        } else {
-            entry.target.classList.remove('is-visible');
-        }
-    });
-});
-animatedElements.forEach(el => observer.observe(el));
+setTimeout(function() {
+    $(".load").addClass("active");
+    setTimeout(function() {
+        $(".load").addClass("open");
+        setTimeout(function() {
+            $(".load").addClass("opacity");
+            setTimeout(function() {
+                $(".load").addClass("none");
+                $("body").removeClass("hidden");
+            }, 500);
+        }, 800);
+    }, 1100);
+}, 100);
 
 // Header Fixed
 $(window).on('scroll', function () {
